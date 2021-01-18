81cbbfa4978970b0fd764c1efd22915bdb4b5e3b
Tigran Grigorian <76774512+tgrig@users.noreply.github.com>
2021-01-18 14:05:59 +0400

    Collect strings

---
+++ Recursion/collectStrings.js
@@ -0,0 +1,40 @@
+// Write a function called&nbsp;<code>collectStrings</code>&nbsp;which accepts an
+// object and returns an array of all the values in the object that have a typeof
+// string
+
+// Example:
+// collectStrings(obj); // ["foo", "bar", "baz"])
+
+const obj = {
+  stuff: "foo",
+  data: {
+    val: {
+      thing: {
+        info: "bar",
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: "baz",
+          },
+        },
+      },
+    },
+  },
+};
+
+function collectStrings(obj) {
+  var stringsArr = [];
+
+  function gatherStrings(o) {
+    for (var key in o) {
+      if (typeof o[key] === "string") {
+        stringsArr.push(o[key]);
+      } else if (typeof o[key] === "object") {
+        return gatherStrings(o[key]);
+      }
+    }
+  }
+
+  gatherStrings(obj);
+
+  return stringsArr;
+}
