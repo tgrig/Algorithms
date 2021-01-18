613d19060ed639096d77d8c86915b67c87170a8f
Tigran Grigorian <76774512+tgrig@users.noreply.github.com>
2021-01-18 14:02:11 +0400

    Product of array

---
+++ Recursion/productOfArray.js
@@ -0,0 +1,13 @@
+// Write a function called productOfArray which takes in an array of numbers
+// and returns the product of them all.
+
+// Examples:
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+
+function productOfArray(arr) {
+  if (arr.length === 0) {
+    return 1;
+  }
+  return arr[0] * productOfArray(arr.slice(1));
+}
